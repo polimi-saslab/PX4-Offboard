6b988b28695ad853ecc388f9833c3d028fe92b93
leonardocecchin <leonardo.cecchin@outlook.it>
2024-12-11 17:06:05 +0100

    launch files

--- px4_ros_bridge/launch/tarot_680_rplidar_launch.py
+++ px4_ros_bridge/launch/tarot_680_rplidar_launch.py
@@ -1,8 +1,10 @@
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 from launch.actions import LogInfo, RegisterEventHandler, ExecuteProcess, IncludeLaunchDescription, TimerAction, DeclareLaunchArgument
-from launch.substitutions import FindExecutable, LaunchConfiguration, PythonExpression
+from launch.substitutions import FindExecutable, LaunchConfiguration, PythonExpression, PathJoinSubstitution, TextSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.conditions import IfCondition
 import os
 
@@ -29,37 +31,19 @@ def generate_launch_description():
         name='odom_broadcaster_node'
     )
 
-    pitot_1_logger_node = Node(
-        package='pitot_logger',
-        executable='pitot_ros_bridge',
-        name='pitot_ros_bridge_node',
-        parameters=[{
-            'frame_id': 'vehicle',
-            'device_name': 'pitot_1'
-        }],
-        # execute only if device pitot_1 is present
-        # condition=IfCondition(
-        #     PythonExpression([
-        #         'os.path.exists("/dev/pitot_1")'
-        #     ])
-        # )
-    )
-
-    pitot_2_logger_node = Node(
-        package='pitot_logger',
-        executable='pitot_ros_bridge',
-        name='pitot_ros_bridge_node',
-        parameters=[{
-            'frame_id': 'vehicle',
-            'device_name': 'pitot_2'
-        }],
-        # execute only if device pitot_2 is present
-        # condition=IfCondition(
-        #     PythonExpression([
-        #         'os.path.exists("/dev/pitot_2")'
-        #     ])
-        # )
-    )
+    rplidar_launch = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('rplidar_ros'),
+                    'launch',
+                    'rplidar_a2m8_launch.py'
+                ])
+            ]),
+            launch_arguments={
+                'serial_port': '/dev/RPlidar',
+                'frame_id': 'laser'
+            }.items()
+        )
 
     return LaunchDescription([   
         DeclareLaunchArgument(
@@ -70,6 +54,5 @@ def generate_launch_description():
 
         micro_XRCE_agent,
         odom_broadcaster_node,
-        pitot_1_logger_node,
-        pitot_2_logger_node,
+        rplidar_launch
     ])
